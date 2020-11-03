0fd085023b2a68817b78cde6f253e1e17bf0ef76
aGro1003 <73796974+aGro1003@users.noreply.github.com>
2020-11-03 19:39:06 +0100

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taschenrechner_Iteration_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Als Benutzer möchte ich zwei Zahlen eingeben, um deren Summe berechnen zu lassen
+            Console.Write("Bitte gib den ersten Summanden ein: ");
+            string ersterSummand = Console.ReadLine();
+            Console.Write("Bitte gib den zweiten Summanden ein: ");
+            string zweiterSummand = Console.ReadLine();
+
+            //Wandel Text in Ganzzahlen
+            int ersterSummandalsZahl = Convert.ToInt32(ersterSummand);
+            int zweiterSummandalsZahl = Convert.ToInt32(zweiterSummand);
+
+            //Berechnung ausführen
+            int summe = ersterSummandalsZahl + zweiterSummandalsZahl;
+
+            Console.WriteLine($"Die Summe beider Summanden ist: {summe}");
+            Console.ReadLine();
+        }
+    }
+}
